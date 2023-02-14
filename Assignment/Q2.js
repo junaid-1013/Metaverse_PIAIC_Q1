f71039927fe00b2ffdb910623bee1d466d234b2e
Junaid Ali Bhatti <junaid.ali101452@gmail.com>
2023-02-14 20:13:08 +0500

    Assignment First Commit

---
+++ Assignment/Q2.js
@@ -0,0 +1,2 @@
+var my_name = 'Junaid';
+console.log("Hello!" + my_name + ", Would you like to learn some python today?");
---
+++ Assignment/Q3.js
@@ -0,0 +1,4 @@
+var my_name = "jUnAid";
+console.log("Name in Upper Case is: " + my_name.toUpperCase());
+console.log("Name in Lower Case is: " + my_name.toLowerCase());
+console.log("Name in Sentence Case is: " + my_name.toLowerCase().charAt(0).toUpperCase() + my_name.slice(1).toLowerCase());
